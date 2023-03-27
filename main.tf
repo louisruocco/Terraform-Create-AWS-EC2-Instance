c609971da8c372072dee56a811436616c61abe00
Louis Ruocco <louisruocco1@gmail.com>
2023-03-27 21:27:09 +0100

    added name tag to resource

--- main.tf
+++ main.tf
@@ -1,17 +1,21 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~>3.5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-073bb7464cc51df7c"
-    instance_type = "t2.micro"
+  ami           = "ami-073bb7464cc51df7c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Lou's Windows Server EC2 Instance."
+  }
 }
